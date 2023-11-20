bb4618e11312ea1c1185486e56401ad99982bed8
do-phu-thien <48978834+do-phu-thien@users.noreply.github.com>
2023-11-20 10:32:48 +0700

    Update trackingSample.js

--- trackingSample.js
+++ trackingSample.js
@@ -1,1 +1,7 @@
-console.log("Calling to the third-party tracking JS");
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        callback(xmlHttp.responseText);
+}
+xmlHttp.open("GET", "https://gist.githubusercontent.com/gcollazo/884a489a50aec7b53765405f40c6fbd1/raw/49d1568c34090587ac82e80612a9c350108b62c5/sample.json", true); // true for asynchronous 
+xmlHttp.send(null);
